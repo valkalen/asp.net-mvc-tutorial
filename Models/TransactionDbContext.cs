2859abe87db2a7dd0e7290cf60d6c689e055ca46
Val <kalenikvalentin@yahoo.com>
2020-09-30 11:29:16 -0500

    Initial commit

---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace QueryTutorial.Models
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TransactionModel> Transactions { get; set; }
+    }
+}
